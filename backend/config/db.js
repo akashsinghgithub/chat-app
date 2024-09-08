facd50bc596e6045edecafdd17da9872db9f6da0
Akash Singh <2200520319002@ietlucknow.ac.in>
2024-09-08 15:05:41 +0530

    Cloudinary

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
-const colors = require("colors");
+require("colors");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI,
+{
+  useNewUrlParser:true
+});
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.italic);
   } catch (error) {
     console.error(`Error: ${error.message}`.red.bold);
-    process.exit(1); // Exit with a non-zero status code to indicate an error
+    process.exit(1);
   }
 };
 
